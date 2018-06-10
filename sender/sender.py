82f60239d0a6479bf9785ed8f75b5b67927a1e3e
ssgskid <ks8171235@naver.com>
2018-06-10 00:49:42 -0700

    add windows crash sender (with exploitable)

--- sender/sender.py
+++ sender/sender.py
@@ -43,14 +43,34 @@ def sendVmToReveiver(fuzzer, fuzzingProgram, fuzzing_os, receiverIP, alias, ping
 	payload = {'fuzzer' : fuzzer, 'fuzzingProgram' : fuzzingProgram, 'fuzzing_os' : fuzzing_os, 'alias' : alias, 'pingPort' : pingPort}
 	r = requests.post(receiverIP+"/vm", data=payload)
 
+def sendCrashToReceiverWin(crashName, fuzzingProgram, receiverIP, exploitable):
+	payload = {'crashName' : crashName, 'fuzzingProgram' : fuzzingProgram, 'exploitable' : exploitable}
+	r = requests.post(receiverIP+"/crash", data=payload)
+
+def searchDirWin(workDir, fuzzingProgram, receiverIP):
+	newPivotTime = pivotTime
+	rootDir = os.listdir(workDir)
+	for exploitableDir in rootDir:									#get files
+		workExploitableDir = os.path.join(workDir, exploitableDir)
+		exploitableDirCreateTime = datetime.datetime.fromtimestamp(os.path.getmtime(workExploitableDir))		#get file created time
+		if(exploitableDirCreateTime > pivotTime):
+			crashDir = os.listdir(workExploitableDir)
+			for crashDirName in crashDir:
+				crashDirCreateTime = datetime.datetime.fromtimestamp(os.path.getmtime(os.path.join(workExploitableDir, crashDirName)))		#get file created time
+				if(crashDirCreateTime > pivotTime):
+					sendCrashToReceiverWin(crashDirName, fuzzingProgram, receiverIP, exploitableDir)
+					print ("crash send success!")
+					newPivotTime = max(newPivotTime, crashDirCreateTime)
+	return newPivotTime
+
 def sendCrashToReceiver(crashName, fuzzingProgram, receiverIP):
-	payload = {'crashName' : crashName, 'fuzzingProgram' : fuzzingProgram}
+	payload = {'crashName' : crashName, 'fuzzingProgram' : fuzzingProgram, 'exploitable' : 'this fuzzer not support'}
 	r = requests.post(receiverIP+"/crash", data=payload)
 
 def searchDir(workDir, fuzzingProgram, receiverIP):
 	newPivotTime = pivotTime
 	filenames = os.listdir(workDir)
-	for crashName in filenames:								#get files
+	for crashName in filenames:									#get files
 		full_crashName = os.path.join(workDir, crashName)		#get file name
 		crashCreateTime = datetime.datetime.fromtimestamp(os.path.getctime(full_crashName))		#get file created time
 		if(crashCreateTime > pivotTime):
@@ -105,4 +125,7 @@ if __name__ == "__main__":
 
 	while True:
 		time.sleep(1)
-		pivotTime = searchDir(workDir, fuzzingProgram, receiverIP)
+		if fuzzing_os == 'W':
+			pivotTime = searchDirWin(workDir, fuzzingProgram, receiverIP)
+		else:
+			pivotTime = searchDir(workDir, fuzzingProgram, receiverIP)
